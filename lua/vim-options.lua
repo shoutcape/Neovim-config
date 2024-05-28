0334276565caebc9b37b99982c8962ba132cbe1c
Ville Kautiainen <kautiainen.ville@gmail.com>
2024-05-28 23:43:11 +0300

    added keymaps to move around selected lines, added transparent background to catppuccin

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,16 +3,24 @@ vim.cmd("set tabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.incsearch = true
+vim.opt.scrolloff = 9
 
 
 -- leader key
-vim.g.mapleader = "å"
+vim.g.mapleader = " "
 
-
+-- keymaps to move between buffers 
 vim.api.nvim_set_keymap('n', '<C-h>', ':bprevious<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-l>', ':bnext<CR>', { noremap = true, silent = true })
+
+--keymap to select whole page in select mode
 vim.keymap.set('v', '<leader>e', 'ggVG', {})
 vim.api.nvim_set_keymap('n', 'S', '<Nop>', { noremap = true, silent = true })
+
+-- keymaps to move around selected lines
+vim.api.nvim_set_keymap('v', 'J', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'K', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
 
 
 -- keymaps for scrolling
@@ -26,6 +34,7 @@ autocmd!
 au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=200})
 augroup END
 ]]
+
 
 vim.api.nvim_create_user_command('Cdh', function()
     local ok, err = pcall(function()
