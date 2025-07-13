6bed5413284fd67170c255cb55b27cfe1d6865a1
Ville Kautiainen <kautiainen.ville@gmail.com>
2025-07-13 17:53:48 +0300

    neovide changes

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,36 +1,54 @@
 if vim.g.neovide then
-  vim.g.neovide_opacity = 0.93
-  vim.g.neovide_background_color = "#2D2A2E"
-  -- vim.g.neovide_cursor_vfx_mode = "railgun"
-  vim.g.gui_font_default_size = 12
-  vim.g.gui_font_size = vim.g.gui_font_default_size
-  vim.g.gui_font_face = "0xProto Nerd Font"
-  vim.g.neovide_text_gamma = 0.8
-  vim.g.neovide_text_contrast = 0.1
-  vim.g.neovide_refresh_rate = 120
-  vim.g.neovide_cursor_animation_length = 0.100
+	vim.g.neovide_opacity = 0.93
+	vim.g.neovide_background_color = "#2D2A2E"
+	-- vim.g.neovide_cursor_vfx_mode = "railgun"
+	vim.g.gui_font_default_size = 12
+	vim.g.gui_font_size = vim.g.gui_font_default_size
+	vim.g.gui_font_face = "0xProto Nerd Font"
+	vim.g.neovide_text_gamma = 0.8
+	vim.g.neovide_text_contrast = 0.1
+	vim.g.neovide_refresh_rate = 120
+	vim.g.neovide_cursor_animation_length = 0.100
 
-  RefreshGuiFont = function()
-    vim.opt.guifont = string.format("%s:h%s",vim.g.gui_font_face, vim.g.gui_font_size)
-  end
+	RefreshGuiFont = function()
+		vim.opt.guifont = string.format("%s:h%s", vim.g.gui_font_face, vim.g.gui_font_size)
+	end
 
-  ResizeGuiFont = function(delta)
-    vim.g.gui_font_size = vim.g.gui_font_size + delta
-    RefreshGuiFont()
-  end
+	ResizeGuiFont = function(delta)
+		vim.g.gui_font_size = vim.g.gui_font_size + delta
+		RefreshGuiFont()
+	end
 
-  ResetGuiFont = function ()
-    vim.g.gui_font_size = vim.g.gui_font_default_size
-    RefreshGuiFont()
-  end
+	ResetGuiFont = function()
+		vim.g.gui_font_size = vim.g.gui_font_default_size
+		RefreshGuiFont()
+	end
 
-  -- Call function on startup to set default value
-  ResetGuiFont()
+	-- Call function on startup to set default value
+	ResetGuiFont()
 
-  -- Keymaps
+	vim.api.nvim_create_autocmd("BufLeave", {
+		callback = function()
+			vim.g.neovide_scroll_animation_length = 0
+			vim.g.neovide_cursor_animation_length = 0
+		end,
+	})
+	vim.api.nvim_create_autocmd("BufEnter", {
+		callback = function()
+			vim.fn.timer_start(70, function()
+				vim.g.neovide_scroll_animation_length = 0.3
+				vim.g.neovide_cursor_animation_length = 0.08
+			end)
+		end,
+	})
 
-  local opts = { noremap = true, silent = true }
+	-- Keymaps
+	local opts = { noremap = true, silent = true }
 
-  vim.keymap.set({'n', 'i'}, "<C-+>", function() ResizeGuiFont(1)  end, opts)
-  vim.keymap.set({'n', 'i'}, "<C-->", function() ResizeGuiFont(-1) end, opts)
+	vim.keymap.set({ "n", "i" }, "<C-+>", function()
+		ResizeGuiFont(1)
+	end, opts)
+	vim.keymap.set({ "n", "i" }, "<C-->", function()
+		ResizeGuiFont(-1)
+	end, opts)
 end
