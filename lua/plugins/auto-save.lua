08dce14d41512f8354114dc933ea95d06a8bb30b
Ville Kautiainen <kautiainen.ville@gmail.com>
2025-07-20 19:07:35 +0300

    fix harpoon bug with autosave

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -1,17 +1,31 @@
 return {
-  "Pocco81/auto-save.nvim",
-  event = "VeryLazy",
-  config = function()
-    require("auto-save").setup({
-      enabled = true,
-      execution_message = {
-        message = function()
-          return ""
-        end,
-      },
-      trigger_events = { "TextChanged" },
-      write_all_buffers = false,
-      debounce_delay = 250,
-    })
-  end,
+	"Pocco81/auto-save.nvim",
+	event = "VeryLazy",
+	config = function()
+		require("auto-save").setup({
+			enabled = true,
+			execution_message = {
+				message = function()
+					return ""
+				end,
+			},
+			trigger_events = { "TextChanged" },
+			condition = function(buf)
+				-- First check if the buffer ID is valid
+				if not vim.api.nvim_buf_is_valid(buf) then
+					return false
+				end
+
+				if vim.bo[buf].filetype == "harpoon" then
+					return false
+				end
+
+				local utils = require("auto-save.utils.data")
+				local ft = vim.bo[buf].filetype
+				return vim.bo[buf].modifiable and utils.not_in(ft, { "harpoon" })
+			end,
+			write_all_buffers = false,
+			debounce_delay = 250,
+		})
+	end,
 }
