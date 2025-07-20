414cd3111bed7fee79875d4c19b8585a641addd2
Ville Kautiainen <kautiainen.ville@gmail.com>
2025-07-20 18:35:28 +0300

    reorder neovide.lua

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,14 +1,16 @@
 if vim.g.neovide then
-	vim.g.neovide_opacity = 0.93
-	vim.g.neovide_background_color = "#2D2A2E"
-	-- vim.g.neovide_cursor_vfx_mode = "railgun"
-	vim.g.gui_font_default_size = 12
-	vim.g.gui_font_size = vim.g.gui_font_default_size
-	vim.g.gui_font_face = "0xProto Nerd Font"
-	vim.g.neovide_text_gamma = 0.8
-	vim.g.neovide_text_contrast = 0.1
-	vim.g.neovide_refresh_rate = 120
-	vim.g.neovide_cursor_animation_length = 0.100
+  vim.g.neovide_background_color = "#2D2A2E"
+  vim.g.gui_font_default_size = 12
+  vim.g.gui_font_size = vim.g.gui_font_default_size
+  vim.g.gui_font_face = "0xProto Nerd Font"
+
+  vim.g.neovide_refresh_rate = 120
+  -- vim.g.neovide_cursor_vfx_mode = "railgun"
+  vim.g.neovide_cursor_animation_length = 0.100
+
+  vim.g.neovide_opacity = 0.93
+  -- vim.g.neovide_text_gamma = 0.8
+  -- vim.g.neovide_text_contrast = 0.1
 
 	RefreshGuiFont = function()
 		vim.opt.guifont = string.format("%s:h%s", vim.g.gui_font_face, vim.g.gui_font_size)
