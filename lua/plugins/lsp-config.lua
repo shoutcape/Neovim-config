292f18562e47bf02fae79503aa2db115b0a66012
Ville Kautiainen <kautiainen.ville@gmail.com>
2025-07-26 17:59:22 +0300

    remove lsp setups from lsp config, mason handles it all

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,114 +1,50 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ui = {
-        border = "rounded",
-      },
-    },
-  },
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ui = {
+				border = "rounded",
+			},
+		},
+	},
 
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = {
-        "lua_ls",
-        "cssls",
-        "html",
-        "jsonls",
-        "pyright",
-        "tailwindcss",
-        "eslint",
-      },
-    },
-  },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {
+				"lua_ls",
+				"cssls",
+				"html",
+				"jsonls",
+				"pyright",
+				"tailwindcss",
+				"eslint",
+			},
+		},
+	},
 
-  {
-    "pmizio/typescript-tools.nvim",
-    ft = { "typescript", "javascript", "typescriptreact", "javascriptreact" },
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {
-      settings = {
-        tsserver_plugins = {
-          "@styled/typescript-styled-plugin",
-        },
-        tsserver_max_memory = "auto",
-        tsserver_format_options = {
-          allowIncompleteCompletions = false,
-          allowRenameOfImportPath = false,
-        },
-        tsserver_file_preferences = {
-          includeInlayParameterNameHints = "all",
-          includeCompletionsForModuleExports = true,
-          quotePreference = "auto",
-        },
-      },
-      server = {}
-    },
-  },
-
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      -- local lspconfig = require("lspconfig")
-
-      -- 🌟 Set custom floating window borders globally
-      vim.diagnostic.config({
-        float = {
-          border = {
-            { "╭", "FloatBorder" },
-            { "─", "FloatBorder" },
-            { "╮", "FloatBorder" },
-            { "│", "FloatBorder" },
-            { "╯", "FloatBorder" },
-            { "─", "FloatBorder" },
-            { "╰", "FloatBorder" },
-            { "│", "FloatBorder" },
-          },
-          source = "always",
-          prefix = function(_, i, total)
-            return string.format(" [%d/%d]", i, total)
-          end,
-        },
-      })
-
-      -- Set up LSP servers manually
-      -- lspconfig.lua_ls.setup({
-      --   settings = {
-      --     Lua = {
-      --       diagnostics = { globals = { "vim" } },
-      --       workspace = { checkThirdParty = false },
-      --       telemetry = { enable = false },
-      --     },
-      --   },
-      -- })
-
-      -- lspconfig.cssls.setup({
-      --   settings = {
-      --     css = {
-      --       lint = { unknownAtRules = "ignore" },
-      --     },
-      --   },
-      -- })
-
-      -- lspconfig.ts_ls.setup({})
-      -- lspconfig.html.setup({})
-      -- lspconfig.jsonls.setup({})
-      -- lspconfig.pyright.setup({})
-      --
-      -- lspconfig.tailwindcss.setup({
-      --   settings = {
-      --     tailwindCSS = {
-      --       lint = { unknownAtRules = "ignore" },
-      --     },
-      --   },
-      -- })
-
-      -- lspconfig.eslint.setup({
-      --   settings = {
-      --     workingDirectory = { mode = "auto" },
-      --   },
-      -- })
-    end,
-  },
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			-- 🌟 Set custom floating window borders globally
+			vim.diagnostic.config({
+				float = {
+					border = {
+						{ "╭", "FloatBorder" },
+						{ "─", "FloatBorder" },
+						{ "╮", "FloatBorder" },
+						{ "│", "FloatBorder" },
+						{ "╯", "FloatBorder" },
+						{ "─", "FloatBorder" },
+						{ "╰", "FloatBorder" },
+						{ "│", "FloatBorder" },
+					},
+					source = "always",
+					prefix = function(_, i, total)
+						return string.format(" [%d/%d]", i, total)
+					end,
+				},
+			})
+		end,
+	},
 }
