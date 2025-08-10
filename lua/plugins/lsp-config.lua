223da419b9f775fb49f5fd6f3dcbed1d9fcc1a2b
Ville Kautiainen <kautiainen.ville@gmail.com>
2025-08-10 21:27:10 +0300

    Add more memory for ts_ls with mason-lspconfig

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,50 +1,67 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		opts = {
-			ui = {
-				border = "rounded",
-			},
-		},
-	},
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ui = { border = "rounded" },
+    },
+  },
 
-	{
-		"williamboman/mason-lspconfig.nvim",
-		opts = {
-			ensure_installed = {
-				"lua_ls",
-				"cssls",
-				"html",
-				"jsonls",
-				"pyright",
-				"tailwindcss",
-				"eslint",
-			},
-		},
-	},
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "lua_ls",
+        "cssls",
+        "html",
+        "jsonls",
+        "pyright",
+        "tailwindcss",
+        "eslint",
+        "ts_ls",
+      },
+      handlers = {
+        -- Default handler (applies to every server unless overridden below)
+        function(server)
+          require("lspconfig")[server].setup({})
+        end,
 
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			-- 🌟 Set custom floating window borders globally
-			vim.diagnostic.config({
-				float = {
-					border = {
-						{ "╭", "FloatBorder" },
-						{ "─", "FloatBorder" },
-						{ "╮", "FloatBorder" },
-						{ "│", "FloatBorder" },
-						{ "╯", "FloatBorder" },
-						{ "─", "FloatBorder" },
-						{ "╰", "FloatBorder" },
-						{ "│", "FloatBorder" },
-					},
-					source = "always",
-					prefix = function(_, i, total)
-						return string.format(" [%d/%d]", i, total)
-					end,
-				},
-			})
-		end,
-	},
+        -- Per-server override(s)
+        ts_ls = function()
+          require("lspconfig").ts_ls.setup({
+            init_options = {
+              -- give the TypeScript server more memory
+              maxTsServerMemory = 8192, -- try 4096 first if 8GiB is too much
+            },
+          })
+        end,
+      },
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      -- custom diagnostic float UI
+      vim.diagnostic.config({
+        float = {
+          border = {
+            { "╭", "FloatBorder" },
+            { "─", "FloatBorder" },
+            { "╮", "FloatBorder" },
+            { "│", "FloatBorder" },
+            { "╯", "FloatBorder" },
+            { "─", "FloatBorder" },
+            { "╰", "FloatBorder" },
+            { "│", "FloatBorder" },
+          },
+          source = "always",
+          prefix = function(_, i, total)
+            return string.format(" [%d/%d]", i, total)
+          end,
+        },
+      })
+      -- no direct server setups here; mason-lspconfig handles those via handlers
+    end,
+  },
 }
+
