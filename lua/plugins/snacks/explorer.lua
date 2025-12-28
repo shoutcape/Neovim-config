e871a5316d478207484692345bafcfa4bef5e845
Shoutcape <kautiainen.ville@gmail.com>
2025-12-28 17:47:58 +0200

    fix

--- lua/plugins/snacks/explorer.lua
+++ lua/plugins/snacks/explorer.lua
@@ -1,48 +1,48 @@
 return {
-  "snacks.nvim",
-  opts = {
-    explorer = {
-      replace_netrw = true, -- Replace netrw with the snacks explorer
-      win = {
-        list = {
-          keys = {
-            ["<BS>"] = "explorer_up",
-            ["l"] = "",
-            ["h"] = "", -- close directory
-            ["a"] = "explorer_add",
-            ["d"] = "explorer_del",
-            ["r"] = "explorer_rename",
-            ["c"] = "explorer_copy",
-            ["m"] = "explorer_move",
-            ["o"] = "explorer_open", -- open with system application
-            ["P"] = "toggle_preview",
-            ["y"] = { "explorer_yank", mode = { "n", "x" } },
-            ["p"] = "explorer_paste",
-            ["u"] = "explorer_update",
-            ["<c-c>"] = "tcd",
-            ["<leader>/"] = "picker_grep",
-            ["<c-t>"] = "terminal",
-            ["."] = "explorer_focus",
-            ["I"] = "toggle_ignored",
-            ["H"] = "toggle_hidden",
-            ["Z"] = "explorer_close_all",
-            ["]g"] = "explorer_git_next",
-            ["[g"] = "explorer_git_prev",
-            ["]d"] = "explorer_diagnostic_next",
-            ["[d"] = "explorer_diagnostic_prev",
-            ["]w"] = "explorer_warn_next",
-            ["[w"] = "explorer_warn_prev",
-            ["]e"] = "explorer_error_next",
-            ["[e"] = "explorer_error_prev",
-          },
-        },
-      },
-    },
+	"snacks.nvim",
+	opts = {
+		explorer = {
+			replace_netrw = true, -- Replace netrw with the snacks explorer
+			win = {
+				list = {
+					keys = {
+						["<BS>"] = "explorer_up",
+						["l"] = "",
+						["h"] = "", -- close directory
+						["a"] = "explorer_add",
+						["d"] = "explorer_del",
+						["r"] = "explorer_rename",
+						["c"] = "explorer_copy",
+						["m"] = "explorer_move",
+						["o"] = "explorer_open", -- open with system application
+						["P"] = "toggle_preview",
+						["y"] = { "explorer_yank", mode = { "n", "x" } },
+						["p"] = "explorer_paste",
+						["u"] = "explorer_update",
+						["<c-c>"] = "tcd",
+						["<leader>/"] = "picker_grep",
+						["<c-t>"] = "terminal",
+						["."] = "explorer_focus",
+						["I"] = "toggle_ignored",
+						["H"] = "toggle_hidden",
+						["Z"] = "explorer_close_all",
+						["]g"] = "explorer_git_next",
+						["[g"] = "explorer_git_prev",
+						["]d"] = "explorer_diagnostic_next",
+						["[d"] = "explorer_diagnostic_prev",
+						["]w"] = "explorer_warn_next",
+						["[w"] = "explorer_warn_prev",
+						["]e"] = "explorer_error_next",
+						["[e"] = "explorer_error_prev",
+					},
+				},
+			},
+		},
 		picker = {
 			sources = {
 				explorer = {
-          hidden = true,
-          ignored = true,
+					hidden = true,
+					ignored = true,
 					layout = {
 						layout = { position = "right" },
 						preview = { enabled = false, main = true },
@@ -51,5 +51,5 @@ return {
 				},
 			},
 		},
-  },
+	},
 }
