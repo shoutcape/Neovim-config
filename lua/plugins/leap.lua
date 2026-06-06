f2b77612133ca5699522b75fff2479438474f115
shoutcape <kautiainen.ville@gmail.com>
2026-06-06 13:51:31 +0300

    update leap.lua keymaps for leap v2 API

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -4,10 +4,7 @@ return {
         "tpope/vim-repeat"
     },
     config = function()
-        vim.keymap.set({'n', 'x', 'o'}, 's',  '<Plug>(leap-forward-to)')
-        vim.keymap.set({'n', 'x', 'o'}, 'S',  '<Plug>(leap-backward-to)')
-        vim.keymap.set({'x', 'o'},      'x',  '<Plug>(leap-forward-till)')
-        vim.keymap.set({'x', 'o'},      'X',  '<Plug>(leap-backward-till)')
-        vim.keymap.set({'n', 'x', 'o'}, 'gs', '<Plug>(leap-from-window)')
+        vim.keymap.set({'n', 'x', 'o'}, 's',  '<Plug>(leap)')
+        vim.keymap.set('n',             'S',  '<Plug>(leap-from-window)')
     end
 }
